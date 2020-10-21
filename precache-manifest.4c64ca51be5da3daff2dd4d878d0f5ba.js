9f04c8ef0cea579bdd1e45db97155e667453fdf0
ezemsm <ezemsm@gmail.com>
2020-10-21 15:15:43 -0500

    Updates

---
+++ precache-manifest.4c64ca51be5da3daff2dd4d878d0f5ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "401b24e6fd45bbf0751dd304beb0b0d5",
+    "url": "/store/index.html"
+  },
+  {
+    "revision": "f82e0acd706510fa97e6",
+    "url": "/store/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "19d0c9b2477bb188efad",
+    "url": "/store/static/js/2.0bfa40de.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/store/static/js/2.0bfa40de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f82e0acd706510fa97e6",
+    "url": "/store/static/js/main.99b61094.chunk.js"
+  },
+  {
+    "revision": "baa40557d47c32a07c05",
+    "url": "/store/static/js/runtime-main.2508a05c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/store/static/media/logo.5d5d9eef.svg"
+  }
+]);
