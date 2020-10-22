3d6b03bc3e064ba28df9ee7065fe572758681ce3
ezemsm <ezemsm@gmail.com>
2020-10-21 22:12:33 -0500

    update 68 lesson 3

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class  App extends Component {
-  
-    state= { meaningOfLife:47+this.props.increment };   
-    //add props incase needed in constructor
-    
-  
+import HomePage from './pages/homepage/homepage.component';
 
-handleClick  = () =>{
-  this.setState((prevState,prevProps) => {
-    return { 
-      meaningOfLife: prevState.meaningOfLife+ prevProps.increment
-    }
-  },  console.log(this.state.meaningOfLife));
-}
-
-  render(){ 
+function App() {
   return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p> {this.state.meaningOfLife} </p>
-  <button onClick={this.handleClick}>Click Me!</button>
-        </header>
-        
-      </div>
-    );
-  }
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
